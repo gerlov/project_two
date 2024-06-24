04c2a869b21367c6319de89aa12b322cd6798b6a
gerlov <gerlov@kth.se>
2024-06-24 21:18:49 +0200

    Initial commit

---
+++ src/main/java/com/kth/project_dollarstore/ProjectDollarstoreApplication.java
@@ -0,0 +1,13 @@
+package com.kth.project_dollarstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDollarstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDollarstoreApplication.class, args);
+	}
+
+}
