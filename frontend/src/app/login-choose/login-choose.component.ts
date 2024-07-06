0b42045b9427e5b69af69782d5092b1851f85f6e
juliazubko <102211232+juliazubko@users.noreply.github.com>
2024-07-06 12:03:11 +0200

    removed old header

--- frontend/src/app/login-choose/login-choose.component.ts
+++ frontend/src/app/login-choose/login-choose.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-choose',
