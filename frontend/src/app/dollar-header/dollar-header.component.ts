c238d74a2abab25fbf4107dbdba1fb6eafcc7b62
juliazubko <zubko@kth.se>
2024-07-20 09:21:56 +0200

    fixed dollar header for screens up to 360px

--- frontend/src/app/dollar-header/dollar-header.component.ts
+++ frontend/src/app/dollar-header/dollar-header.component.ts
@@ -4,8 +4,16 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-dollar-header',
   templateUrl: './dollar-header.component.html',
-  styleUrl: './dollar-header.component.css'
+  styleUrls: ['./dollar-header.component.css']
 })
 export class DollarHeaderComponent {
   constructor(public router: Router) { }
-}
+
+  toggleMenu(): void {
+    const menu = document.getElementById('bottom-bar');
+    if (menu) {
+      menu.style.left = menu.style.left === '0px' ? '-100%' : '0px';
+    }
+  }
+  
+}  
