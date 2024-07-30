5bcb64e395bd971ee15b7754f5949ff983611329
CJayp <cjaypoblete@hotmail.se>
2024-07-30 13:27:04 +0200

    The repository interfaces are documented

--- backend/src/main/java/com/kth/project_dollarstore/repository/DeleteReasonRepository.java
+++ backend/src/main/java/com/kth/project_dollarstore/repository/DeleteReasonRepository.java
@@ -5,5 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.kth.project_dollarstore.model.DeleteReason;
 
+/**
+ * Repository interface for handling {@link DeleteReason} entities.
+ * Extends {@link JpaRepository} to provide basic CRUD operations for {@link DeleteReason}.
+ */
 @Repository
 public interface DeleteReasonRepository extends JpaRepository<DeleteReason, Integer> {}
